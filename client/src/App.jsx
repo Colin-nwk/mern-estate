84ab3584163193b15702a22d12cb7b682c233b8e
Collins Nwoko C <collinsnwoko05@gmail.com>
2023-10-12 18:07:45 +0100

    pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,23 @@
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import SignOut from "./pages/SignOut";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <>
-      <h1 className="text-blue-900 uppercase">Vite + React</h1>
-    </>
+    <Routes>
+      <Route path="/" index element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/signout" element={<SignOut />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
 
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,7 @@
+const Home = () => {
+  return (
+    <div className="uppercase text-3xl h-screen w-screen bg-red-300">Home</div>
+  );
+};
+
+export default Home;
